1f744312aeb7a5a3d3ca3d65dd1148d8cfee8b45
lf-iacob <iacob.laura.f@gmail.com>
2024-12-15 22:52:03 +0100

    Create esperienza5_garageband_handmade.py

---
+++ esp5/esperienza5_garageband_handmade.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def fft_handmade(X):
+    '''
+    FUNZIONE FFT_HANDMADE
+    Funzione che, passato l'array di coefficienti estratti mediante la trasformata di Fourier X,
+    si occupa di calcolare l'antitrasformare il segnale (ovvero di re-sintetizzarlo) come somma
+    finita di coseni e seni con ciascuno il suo peso e sfasamento. Si usa la formula di sintesi
+    definita per il segnale campionato nel finito.
+    '''
+    signal=np.empty(0)
+    N=len(X)
+    for n in range(0, N):
+        sum=0
+        for k in range(0, N):
+            sum=sum+(X[k]*(np.cos((2*np.pi*n*k)/N)+1j*np.sin((2*np.pi*n*k)/N)))
+        signal=np.append(signal, sum)
+    return signal
