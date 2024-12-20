2990f5c3f6d96ae23d976aee7830e4c8f51783de
lf-iacob <iacob.laura.f@gmail.com>
2024-12-20 15:31:28 +0100

    Update aa.py

--- esp5/aa.py
+++ esp5/aa.py
@@ -11,7 +11,7 @@ default_speaker.play(data1/np.max(data1), samplerate1)     #Riproduzione del fil
 #Estrazione dei dati dei file .wav
 y1=data1[:,0]
 num1=len(y1)
-x1=np.linspace(0 , num1/samplerate1, num1)
+t1=np.linspace(0 , num1/samplerate1, num1)
 
 #Riscrittura in file .wav
 sf.write('./pulita_pezzo_rw.wav', y1, samplerate1)
