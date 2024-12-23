6325b714e4e3067f3aeef35bfc870ae34a47e908
lf-iacob <iacob.laura.f@gmail.com>
2024-12-23 18:33:58 +0100

    Update esperienza5_garageband_2b1_pulitadifficile.py

--- esp5/esperienza5_garageband_2b1_pulitadifficile.py
+++ esp5/esperienza5_garageband_2b1_pulitadifficile.py
@@ -18,89 +18,81 @@ y2=data2[:,0]
 num2=len(y2)
 t2=np.linspace(0 , num2/samplerate2, num2)
 
+
 #Realizzazione del grafico: waveform
 plt.subplots(figsize=(11,7))
 plt.plot(t2, y2, color='red')
 plt.title('Waveform 4: Pulita difficile', size=30)
-plt.xlabel('Tempo [s]', size=20)
-plt.ylabel('Ampiezza [UA]', size=20)
+plt.xlabel('Tempo (s)', size=20)
+plt.ylabel('Ampiezza (UA)', size=20)
 plt.show()
 default_speaker.play(data2/np.max(data2), samplerate2) #ascolto originale
 
-
 ''' ANALISI DI FOURIER '''
-
 #Trasformata e normalizzazione
 c2=fft.fft(y2)
 freq2=fft.fftfreq(len(y2), d=(t2[1]-t2[0]))
 r2=c2.real #parte reale
 i2=c2.imag #parte immaginaria
 p2=abs(c2)**2 #potenza
-'''
-df=pd.DataFrame(columns=['p2']) #dataframe per vedere come sono fatte le potenze
-df['p2']=p2
-df.to_csv('why.csv')
-'''
-mask_norm=p2>=max(p2) #maschera di normalizzazione al picco più alto
-c2_0=c2.copy()
-c2_0=c2_0[mask_norm]
-p2_0=abs(c2_0[0])**2
+
+p2_0=max(p2)
 p2_norm=p2/p2_0
 
 fig, ax=plt.subplots(1, 3, figsize=(15,6))
-fig.suptitle('Fourier Analysis: Pulita difficile', fontsize=35, y=1.05)
+fig.suptitle('Fourier Analysis: Pulita difficile', fontsize=25, y=1)
 ax[0].set_title('Parte reale', fontsize=25)
 ax[0].plot(freq2[:len(freq2)//2], r2[:len(r2)//2], color='red')
 ax[0].grid(linestyle=':')
-ax[0].set_xlabel('Frequenza [Hz]', fontsize=20)
-ax[0].set_ylabel('Re(C_k) [UA]', fontsize=20)
+ax[0].set_xlabel('Frequenza (Hz)', fontsize=20)
+ax[0].set_ylabel('Re(C_k) (UA)', fontsize=20)
 ax[1].set_title('Parte immaginaria', fontsize=25)
 ax[1].plot(freq2[:len(freq2)//2], i2[:len(i2)//2], color='red')
 ax[1].grid(linestyle=':')
-ax[1].set_xlabel('Frequenza [Hz]', fontsize=20)
-ax[1].set_ylabel('Im(C_k) [UA]', fontsize=20)
+ax[1].set_xlabel('Frequenza (Hz)', fontsize=20)
+ax[1].set_ylabel('Im(C_k) (UA)', fontsize=20)
 ax[2].set_title('Spettro di potenza', fontsize=25)
 ax[2].plot(freq2[:len(freq2)//2], p2[:len(p2)//2], color='red')
 ax[2].grid(linestyle=':')
-ax[2].set_xlabel('Frequenza [Hz]', fontsize=20)
-ax[2].set_ylabel('|C_k|^2 [UA]', fontsize=20)
+ax[2].set_xlabel('Frequenza (Hz)', fontsize=20)
+ax[2].set_ylabel('|C_k|^2 (UA)', fontsize=20)
 plt.show()
 
 plt.figure()
 plt.title('Pulita difficile: Spettro di potenza normalizzato', fontsize=25)
 plt.plot(freq2[:len(freq2)//2], p2_norm[:len(p2)//2], color='red', marker='*')
 plt.grid(linestyle=':')
-#plt.xlim(-50, 2500)
-plt.xlabel('Frequenza [Hz]', fontsize=20)
+plt.xlim(-50, 3500)
+plt.xlabel('Frequenza (Hz)', fontsize=20)
 plt.ylabel('|C_k|^2 norm', fontsize=20)
 plt.show()
 
-''' PARTE DA AGGIUSTARE CON I LIMITI DELLE SCALE '''
 # Larghezza dei picchi come intervallo di frequenze
-fig, ax=plt.subplots(1, 3, figsize=(15,6))
-fig.suptitle('Larghezza di picchi principali: Pulita difficile', fontsize=35, y=1.05)
-ax[0].set_title('Primo picco', fontsize=25)
-ax[0].plot(freq2[:len(freq2)//2], p2_norm[:len(r2)//2], color='red', marker='*')
+fig, ax=plt.subplots(1, 4, figsize=(18,6))
+fig.suptitle('Larghezza picco armonica fondamentale', fontsize=25, y=1)
+ax[0].set_title('Mi', fontsize=25)
+ax[0].plot(freq2[:len(freq2)//2], p2_norm[:len(p2)//2], color='red', marker='*')
 ax[0].grid(linestyle=':')
-ax[0].set_xlabel('Frequenza [Hz]', fontsize=20)
+ax[0].set_xlabel('Frequenza (Hz)', fontsize=20)
 ax[0].set_ylabel('|C_k|^2 norm', fontsize=20)
-ax[0].set_xlim(-0.5, 2)
-ax[1].set_title('Secondo picco', fontsize=25)
-ax[1].plot(freq2[:len(freq2)//2], p2_norm[:len(i2)//2], color='red', marker='*')
+ax[0].set_xlim(81, 84)
+ax[0].set_ylim(-0.005, 0.075)
+ax[1].set_title('La', fontsize=25)
+ax[1].plot(freq2[:len(freq2)//2], p2_norm[:len(p2)//2], color='red', marker='*')
 ax[1].grid(linestyle=':')
-ax[1].set_xlabel('Frequenza [Hz]', fontsize=20)
-ax[1].set_ylabel('|C_k|^2 norm', fontsize=20)
-ax[1].set_xlim(100, 1300)
-ax[1].set_ylim(0, 0.75)
-ax[2].set_title('Terzo picco', fontsize=25)
+ax[1].set_xlabel('Frequenza (Hz)', fontsize=20)
+ax[1].set_xlim(108, 112)
+ax[1].set_ylim(-0.01, 0.195)
+ax[2].set_title('Si', fontsize=25)
 ax[2].plot(freq2[:len(freq2)//2], p2_norm[:len(p2)//2], color='red', marker='*')
 ax[2].grid(linestyle=':')
-ax[2].set_xlabel('Frequenza [Hz]', fontsize=20)
-ax[2].set_ylabel('|C_k|^2 norm', fontsize=20)
-ax[2].set_xlim(2095, 2115)
-ax[2].set_ylim(0, 0.015)
+ax[2].set_xlim(245, 251)
+ax[2].set_ylim(-0.03, 0.63)
+ax[2].set_xlabel('Frequenza (Hz)', fontsize=20)
+ax[3].set_title('Do#', fontsize=25)
+ax[3].plot(freq2[:len(freq2)//2], p2_norm[:len(p2)//2], color='red', marker='*')
+ax[3].grid(linestyle=':')
+ax[3].set_xlim(276, 279)
+ax[3].set_ylim(-0.007, 0.08)
+ax[3].set_xlabel('Frequenza (Hz)', fontsize=20)
 plt.show()
-
-''' PARTE MANCANTE: FILTRAGGIO DEI PICCHI (QUALI?) E SINTENTIZZAZIONE DEL SEGNALE FILTRETO '''
-
-''' PARTE MANCANTE: RICOSTRUZIONE DEL SEGNALE A MANO COME SOMMA DI SENI E COSENI '''
